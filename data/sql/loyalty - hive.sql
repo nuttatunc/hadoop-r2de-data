8e9425ec1e1ed7a3cc5ec8016a598594ad33252b
nuttatunc <75979952+nuttatunc@users.noreply.github.com>
2022-01-28 23:16:31 +0700

    Delete data directory

--- data/sql/loyalty - hive.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTERNAL TABLE loyalty(
-cust_id int
-,cust_name string
-,cust_member_card_no string
-,spnd_amt int
-) 
-PARTITIONED BY (data_dt string)
-STORED AS PARQUET
-LOCATION'hdfs://quickstart.cloudera:8020/tmp/default/loyalty/'
